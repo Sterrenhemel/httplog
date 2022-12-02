ea41275c28a8aabd3453ad75757eb4b8ff3f28a6
pengdaqian <pengdaqian.0@bytedance.com>
2022-12-03 00:59:20 +0800

    fix http

--- main.go
+++ main.go
@@ -16,12 +16,9 @@ import (
 )
 
 var anyHandler = func(c *gin.Context) {
-	another := c.Request.Clone(c.Request.Context())
 
-	curlCommand, err := http2curl.GetCurlCommand(another)
-	another.Host = "redpanda-cluster-0.redpanda-cluster.redpanda-cluster-default.svc.cluster.local:8081"
-	another.URL = c.Request.URL
-	another.URL.Host = another.Host
+	curlCommand, err := http2curl.GetCurlCommand(c.Request)
+	//c.Request.Host = "redpanda-cluster-0.redpanda-cluster.redpanda-cluster-default.svc.cluster.local:8081"
 	logs.CtxInfow(c.Request.Context(), "curl", "curl", curlCommand.String())
 
 	remote, err := url.Parse("http://redpanda-cluster-0.redpanda-cluster.redpanda-cluster-default.svc.cluster.local:8081")
